f588104aa0c62560b9778a178d82e52552f8b11d
AchinthaHarshamal <achinthafharshamal@gmail.com>
2024-11-20 23:24:42 +0530

    improve button : test

--- qz_app/app/test/page.tsx
+++ qz_app/app/test/page.tsx
@@ -3,12 +3,32 @@ import { Button } from "@/components/ui/button";
 import { useState } from "react";
 
 const TestComponent = () => {
-    const [boderColor, setBorderColor] = useState("");
+    const [borderColor, setBorderColor] = useState("");
     const [hoverColor, setHoverColor] = useState("");   
+    const [bgColor, setBgColor] = useState("");
+    const [textColor, setTextColor] = useState("");
 
-    const handleClick = () => {
+    const handleClick_1 = () => {
         setBorderColor("border-green-500");
         setHoverColor("hover:bg-green-200");
+    };
+    
+    const handleClick_2 = () => {
+        setHoverColor("hover:bg-emerald-100 hover:text-emerald-600");
+        setBgColor("bg-emerald-50");
+        setTextColor("text-emerald-600");
+    };
+    const handleClick_3 = () => {
+        setHoverColor("hover:bg-red-200 hover:text-red-600");
+        setBgColor("bg-red-100");
+        setTextColor("text-red-600");
+    };
+
+    const handleReset = () => {
+        setBorderColor("");
+        setHoverColor("");
+        setBgColor("");
+        setTextColor("");
     };
 
     return (
@@ -17,10 +37,32 @@ const TestComponent = () => {
             <p>This is a test component.</p>
             <Button 
                 variant="ghost" 
-                className={`border-2 border-gray-400 ${boderColor}  ${hoverColor}`} 
-                onClick={handleClick}
+                className={`border-2 border-gray-400 ${borderColor}  ${hoverColor}`} 
+                onClick={handleClick_1}
             >
                 hello
+            </Button>
+            <Button 
+                variant="ghost" 
+                className={`text-base  ${hoverColor} ${bgColor} ${textColor}`} 
+                onClick={handleClick_2}
+            >
+                Correct
+            </Button>
+            
+            <Button 
+                variant="ghost" 
+                className={`text-base  ${hoverColor} ${bgColor} ${textColor}`} 
+                onClick={handleClick_3}
+            >
+                Incorrect
+            </Button>
+            <Button 
+                variant="ghost" 
+                className="border-2 border-gray-400" 
+                onClick={handleReset}
+            >
+                Reset
             </Button>
         </div>
     );
