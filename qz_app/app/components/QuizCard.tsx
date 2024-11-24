c1e83971d1d42b2c943d454a184a43a55b146852
AchinthaHarshamal <achinthafharshamal@gmail.com>
2024-11-25 01:24:35 +0530

    improve quiz card

--- qz_app/app/components/QuizCard.tsx
+++ qz_app/app/components/QuizCard.tsx
@@ -15,13 +15,21 @@ const QuizCard: React.FC<QuizCardProps> = ({ quiz }) => {
   };
 
   return (
-    <Card className="quiz-card" onClick={handleClick} style={{ cursor: "pointer" }}>
-      <CardHeader>
+    <Card
+      className="quiz-card hover:shadow-lg transition-shadow duration-300 "
+      onClick={handleClick}
+      style={{ cursor: "pointer" }}
+    >
+      <CardHeader >
         <CardTitle>{quiz.topic}</CardTitle>
       </CardHeader>
       <CardContent>
-        <p>Quiz ID: {quiz.id}</p>
-        <p>Number of Questions: {quiz.questionIds.length}</p>
+        <p className="quiz-id">
+          <span className="font-bold text-gray-700">Quiz ID:</span> <span className="text-gray-500">{quiz.id}</span>
+        </p>
+        <p className="question-count">
+          <span className="font-bold text-gray-700">Number of Questions:</span> <span className="text-gray-500">{quiz.questionIds.length}</span>
+        </p>
       </CardContent>
     </Card>
   );
